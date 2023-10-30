d05e4e96cdca571006fb30cd966682d4ee253b8c
Gabriel Lemyre <gabriel@quotemachine.com>
2023-10-30 10:53:48 -0400

    Add random alt logos in sidebar

--- src/components/Common/Logo.tsx
+++ src/components/Common/Logo.tsx
@@ -1,4 +1,8 @@
-import logo from '../../assets/images/logo-icon.png';
+import logo from '../../assets/images/logo.png';
+import logoAlt1 from '../../assets/images/logo-alt-1.png';
+import logoAlt2 from '../../assets/images/logo-alt-2.png';
+import logoAlt3 from '../../assets/images/logo-alt-3.png';
+import { useEffect, useState } from 'react';
 
 interface LogoProps {
   interactive?: boolean;
@@ -6,17 +10,26 @@ interface LogoProps {
 }
 
 function Logo({ interactive, onClick }: LogoProps) {
+  const [rdmLogo, setRdmLogo] = useState<string>(logo);
+
+  useEffect(() => {
+    if (Math.random() >= 0.9) {
+      const alts = [logoAlt1, logoAlt2, logoAlt3];
+      setRdmLogo(alts[Math.floor(Math.random() * alts.length)])
+    }
+  }, []);
+
   return (
     interactive ? (
       <div
         className='Logo interactive'
         onClick={onClick}
       >
-        <img src={logo} alt='Logo Icon' />
+        <img src={rdmLogo} alt='Logo Icon' />
       </div>
     ) : (
       <div className='Logo'>
-        <img src={logo} alt='Logo Icon' />
+        <img src={rdmLogo} alt='Logo Icon' />
       </div>
     )
   );
