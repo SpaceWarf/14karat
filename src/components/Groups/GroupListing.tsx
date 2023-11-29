a6d3b7e50a6bc15d1b913003b3eb03cbec7f9a02
Space_warf <space.warf@gmail.com>
2023-11-28 22:01:22 -0500

    Add filter to group listing

--- src/components/Groups/GroupListing.tsx
+++ src/components/Groups/GroupListing.tsx
@@ -6,26 +6,35 @@ import Loading from "../Common/Loading";
 import GroupCard from "./GroupCard";
 import { useNavigate } from "react-router-dom";
 import { Group } from "../../state/groups";
+import Filters, { FilterData } from "../Common/Filters";
 
 function GroupListing() {
   const [loading, setLoading] = useState<boolean>(false);
   const [groups, setGroups] = useState<Group[]>([]);
+  const [filteredGroups, setFilteredGroups] = useState<Group[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchGroups = async () => {
       setLoading(true);
-      setGroups(await getItems<Group>(DatabaseTable.GROUPS));
+      const groups = await getItems<Group>(DatabaseTable.GROUPS);
+      setGroups(groups);
+      setFilteredGroups(groups);
       setLoading(false);
     }
     fetchGroups();
   }, []);
+
+  const handleFiltersUpdate = (update: FilterData) => {
+    setFilteredGroups(groups.filter(group => group.name.toLowerCase().includes(update.search.toLowerCase())))
+  }
 
   return (
     <div className="GroupListing">
       <Header text="Group Listing" decorated />
       <div className="content">
         <div className="actions">
+          <Filters tags={[]} onUpdate={handleFiltersUpdate} />
           <button className="ui button positive hover-animation" onClick={() => navigate("/members")}>
             <p className='label contrast'>View All Members</p>
             <p className='IconContainer contrast'><i className='group icon'></i></p>
@@ -35,8 +44,8 @@ function GroupListing() {
           <Loading />
         ) : (
           <div className="CardsContainer">
-            {groups.filter(group => group.cardColor).map(group => <GroupCard group={group} groups={groups} />)}
-            {groups.filter(group => !group.cardColor).map(group => <GroupCard group={group} groups={groups} />)}
+            {filteredGroups.filter(group => group.cardColor).map(group => <GroupCard group={group} groups={groups} />)}
+            {filteredGroups.filter(group => !group.cardColor).map(group => <GroupCard group={group} groups={groups} />)}
             <div
               className='AddGroupCard ui link card attached'
               onClick={() => navigate('/groups/new')}
