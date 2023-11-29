1d08a4bae77c6d53dc3789ac2ff7b68d108aed39
Space_warf <space.warf@gmail.com>
2023-11-28 22:05:08 -0500

    Add filter to members listing

--- src/components/Groups/MembersListing.tsx
+++ src/components/Groups/MembersListing.tsx
@@ -6,20 +6,28 @@ import Loading from "../Common/Loading";
 import { useNavigate } from "react-router-dom";
 import { Member } from "../../state/member";
 import MemberCard from "./MemberCard";
+import Filters, { FilterData } from "../Common/Filters";
 
 function MembersListing() {
   const [loading, setLoading] = useState<boolean>(false);
   const [members, setMembers] = useState<Member[]>([]);
+  const [filteredMembers, setFilteredMembers] = useState<Member[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchMembers = async () => {
       setLoading(true);
-      setMembers(await getItems<Member>(DatabaseTable.MEMBERS));
+      const members = await getItems<Member>(DatabaseTable.MEMBERS);
+      setMembers(members);
+      setFilteredMembers(members);
       setLoading(false);
     }
     fetchMembers();
   }, []);
+
+  const handleFiltersUpdate = (update: FilterData) => {
+    setFilteredMembers(members.filter(member => member.name.toLowerCase().includes(update.search.toLowerCase())))
+  }
 
   return (
     <div className="MembersListing">
@@ -27,16 +35,17 @@ function MembersListing() {
       <div className="content">
         <div className="actions">
           <p className="hyperlink-button" onClick={() => navigate('/groups')}><i className='arrow left icon' /> back</p>
+          <Filters tags={[]} onUpdate={handleFiltersUpdate} />
         </div>
         {loading ? (
           <Loading />
         ) : (
           <div className="CardsContainer">
-            {members.sort((a, b) => a.name.localeCompare(b.name)).map(member => <MemberCard member={member} />)}
-            {/* <div
+            {filteredMembers.sort((a, b) => a.name.localeCompare(b.name)).map(member => <MemberCard member={member} />)}
+            <div
               className='AddGroupCard ui link card attached'
-              onClick={() => navigate('/groups/new')}
-            ><i className="add icon" /></div> */}
+              onClick={() => navigate('/members/new')}
+            ><i className="add icon" /></div>
           </div>
         )}
       </div>
