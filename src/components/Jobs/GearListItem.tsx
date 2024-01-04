e1a5865f3a8a141f3d74e8af5c56824efcb87257
Gabriel Lemyre <gabriel@quotemachine.com>
2024-01-03 20:30:02 -0500

    Load images only on hover

--- src/components/Jobs/GearListItem.tsx
+++ src/components/Jobs/GearListItem.tsx
@@ -1,7 +1,7 @@
-import { Popup, PopupContent } from "semantic-ui-react";
+import { Placeholder, PlaceholderImage, Popup, PopupContent } from "semantic-ui-react";
 import { getGearUrl } from "../../utils/storage";
 import "./Jobs.scss";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
@@ -13,37 +13,36 @@ interface GearListItemProps {
 
 function GearListItem(props: GearListItemProps) {
   const [url, setUrl] = useState<string>();
-  const [loading, setLoading] = useState<boolean>(false);
   const { gear } = useSelector((state: RootState) => state.jobs);
-
-  useEffect(() => {
-    const fetchImage = async () => {
-      setLoading(true);
-      const url = await getGearUrl(props.id)
-      setUrl(url);
-      setLoading(false);
-    }
-    fetchImage();
-  }, [props.id]);
 
   const getName = (): string => {
     return gear.find(g => g.id === props.id)?.name ?? props.id;
+  }
+
+  const handleOpen = async () => {
+    setUrl(await getGearUrl(props.id));
   }
 
   return (
     <div className='GearListItem'>
       {props.bulletted && <li>{props.quantity} {getName()}</li>}
       {!props.bulletted && <p>{props.quantity} {getName()}</p>}
-      {!loading && url && (
-        <Popup
-          trigger={<i className='info circle icon' />}
-          basic
-        >
-          <PopupContent>
+      <Popup
+        trigger={<i className='info circle icon' />}
+        onOpen={handleOpen}
+        basic
+      >
+        <PopupContent>
+          {url && (
             <img src={url} alt={`Gear ${getName()}`} />
-          </PopupContent>
-        </Popup>
-      )}
+          )}
+          {!url && (
+            <Placeholder style={{ height: 150, width: 150 }}>
+              <PlaceholderImage />
+            </Placeholder>
+          )}
+        </PopupContent>
+      </Popup>
     </div>
   );
 }
